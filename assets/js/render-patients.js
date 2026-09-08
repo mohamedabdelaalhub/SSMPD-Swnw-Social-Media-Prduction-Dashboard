37d40d782e4da7eb1ae86ea5250b3030bb8c4f3a
mohamedabdelaalhub <mohamadmh32@gmail.com>
2026-09-09 00:48:02 +0400

    Add files via upload

--- assets/js/render-patients.js
+++ assets/js/render-patients.js
@@ -71,7 +71,7 @@
 
   var state = {
     subTab: "dashboard",
-    browseSearch: "", browsePage: 1, browsePageSize: 2000,
+    browseSearch: "", browsePage: 1, browsePageSize: 20000,
     browseDateField: "created_at", browseDateFrom: "", browseDateTo: "",
     reviewFilter: "pending", reviewPage: 1,
     uploadPatient: null, uploadSearch: "", uploadResults: []
